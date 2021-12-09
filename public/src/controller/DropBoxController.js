3d2b80275d3732b8323352531b0575e8fe7d7988
HKprogram <ricknogueira1231@gmail.com>
2021-12-09 16:18:02 -0300

    Criando Controller e Evento Click no botão Enviar Arquivos

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -17,7 +17,7 @@ class DropBoxController {
 
         });
 
-        this.inputFilesEl.addEventListener('change', event => {
+        this.inputFilesEl.addEventListener('change', event => { 
 
             console.log(event.target.files);
 
