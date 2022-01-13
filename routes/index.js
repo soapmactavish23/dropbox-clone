e111723760b830ce9393a1e81466291563558e98
HKprogram <ricknogueira1231@gmail.com>
2022-01-13 15:59:48 -0300

    Recebendo arquivos no Node.js com Formidable

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post("/upload", (req, res) => {
+
+  const form = new formidable.IncomingForm();
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json({ files });
+  });
 });
 
 module.exports = router;
